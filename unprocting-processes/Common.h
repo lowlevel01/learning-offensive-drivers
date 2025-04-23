53e73a9f24350055054158ce079c397e6bab6c30
Zakariaa <88563786+lowlevel01@users.noreply.github.com>
2025-04-23 16:02:40 +0100

    Create Common.h

---
+++ unprocting-processes/Common.h
@@ -0,0 +1,18 @@
+struct TargetProcess
+{
+    int ProcessId;
+};
+
+typedef struct _PS_PROTECTION
+{
+    UCHAR Type : 3;
+    UCHAR Audit : 1;
+    UCHAR Signer : 4;
+} PS_PROTECTION, * PPS_PROTECTION;
+
+typedef struct _PROCESS_PROTECTION_INFO
+{
+    UCHAR SignatureLevel;
+    UCHAR SectionSignatureLevel;
+    PS_PROTECTION Protection;
+} PROCESS_PROTECTION_INFO, * PPROCESS_PROTECTION_INFO;
